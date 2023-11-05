4e929932b3bdc86f059a3fbf46fab3b51e86dbc9
hassan <hk5708102@gmail.com>
2023-11-05 22:04:04 +0200

    Home Screen Part 1

---
+++ lib/Feature/home/presentation/views/home_view.dart
@@ -0,0 +1,14 @@
+import 'package:aid_humanity/Feature/home/presentation/views/widgets/home_view_body.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold
+      (
+      body:HomeViewBody(),
+    );
+  }
+}
