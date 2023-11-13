0dc3ab4a6eb3cefeff44cbc73a7556d586480de6
hassan <hk5708102@gmail.com>
2023-11-13 23:55:51 +0200

    default_elevated_button

---
+++ lib/core/widgets/default_elevated_button.dart
@@ -0,0 +1,33 @@
+import 'package:aid_humanity/core/extensions/mediaquery_extension.dart';
+import 'package:flutter/material.dart';
+
+class DefaultElevatedButton extends StatelessWidget {
+
+  final double ?width;
+  final double ?height;
+  final VoidCallback onPressed;
+  final String text;
+  final ButtonStyle style;
+
+
+
+  const DefaultElevatedButton({Key? key,this.width,this.height, required this.onPressed, required this.text, required this.style, }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: height,              //context.getDefaultSize() * 3.0
+        width: width,
+        child: ElevatedButton(
+          style:style,
+          onPressed: onPressed,
+          child: Text(text,style: TextStyle(
+            fontSize: context.getDefaultSize() * 1.78,
+          )),
+
+
+        ));
+  }
+}
+
+
+//ElevatedButton.styleFrom(shape:RoundedRectangleBorder(borderRadius:BorderRadius.circular(radius),))
