0f55ebf4f8906ca676d336d3aad46793c5ca202b
hassan <hk5708102@gmail.com>
2023-12-17 15:02:58 +0200

    card delivery & merge firebase

---
+++ lib/Features/home/presentation/widgets/home_delivery_widgets/delivery_tab_buttons.dart
@@ -0,0 +1,94 @@
+import 'package:aid_humanity/Features/home/presentation/widgets/home_delivery_widgets/card_delivery_widget.dart';
+import 'package:aid_humanity/core/extensions/mediaquery_extension.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+class DeliveryTabButtons extends StatelessWidget {
+  const DeliveryTabButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Padding(
+        padding:  EdgeInsets.only(top: context.getDefaultSize() * 2),
+        child: DefaultTabController(
+          length: 3,
+          child: NestedScrollView(
+            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  backgroundColor:Colors.white,
+                  title: const Text('Aid Humanity',style:TextStyle(color: Color(0xFFF8B145))),
+                  actions:
+                  [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          FontAwesomeIcons.magnifyingGlass,
+                          color: Colors.black,
+                          size: 20,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.notifications,
+                          color: Colors.black,
+                        )),
+                  ],
+                  pinned: true,
+                  floating: true,
+                  flexibleSpace: FlexibleSpaceBar
+                    (
+                    background: Container(color:Colors.white,),
+
+                  ),
+                  bottom: const TabBar(
+                    labelColor: Color(0xFFF8B145),
+                    unselectedLabelColor: Colors.black,
+                    physics: ClampingScrollPhysics(),
+                    indicatorColor: Color(0xFFF8B145),
+                    indicatorSize: TabBarIndicatorSize.label,
+                    tabs: [
+                      Tab(child: Text('Requests',)),
+                      Tab(child: Text('History',)),
+                      Tab(child: Text('Live',),),
+
+                    ],
+                  ),
+                ),
+              ];
+            },
+            body:   TabBarView(
+              children: [
+                ListView.builder(
+                  // make scroll in the same position if you are going to another screen and come back
+                  key: const PageStorageKey<String>('CardDeliverWidget'),
+                  itemBuilder: (context, index) {
+                    return CardDeliverWidget() ;
+                  },
+
+                ),
+                ListView.builder(
+                  key: const PageStorageKey<String>('CardDeliver'),
+                  itemBuilder: (context, index) {
+                    return CardDeliverWidget();
+                  },
+
+
+                ),
+                ListView.builder(
+                  key: const PageStorageKey<String>('Widget'),
+                  itemBuilder: (context, index) {
+                    return CardDeliverWidget();
+                  },
+
+                ),
+              ],
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
-import 'package:aid_humanity/Features/home/presentation/pages/home_page.dart';
-import 'package:aid_humanity/core/utils/Localization/app_localization_setup.dart';
-import 'package:aid_humanity/core/widgets/BottomNavigation.dart';
 
+import 'package:aid_humanity/core/utils/Localization/app_localization_setup.dart';
+import 'package:aid_humanity/core/utils/app_router/app_router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'core/utils/theme/theme_data/theme_data_light.dart';
 
-void main() {
-  runApp(const MyApp());
+
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,15 +18,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig:AppRouter.router,
       debugShowCheckedModeBanner: false,
       supportedLocales: AppLocalizationsSetup.supportedLocales, // this line to provie , which langs to use in our app
       localizationsDelegates: AppLocalizationsSetup.localizationsDelegates,
       localeResolutionCallback: (deviceLocale, supportedLocales) {
         return AppLocalizationsSetup.localeResolutionCallback(deviceLocale!, supportedLocales);
       },
-      theme: getThemeDataLight,
-      home: const BottomNavigation(), //const HomeView(),
+      theme: getThemeDataLight, //const HomeView(),
     );
   }
 }
