df4a5300f6d0506dad685e81381d51886988093f
hassan <hk5708102@gmail.com>
2024-03-09 00:45:34 +0200

    Polishing

--- lib/Features/auth/domain/repositeries/auth_repo.dart
+++ lib/Features/auth/domain/repositeries/auth_repo.dart
@@ -4,6 +4,6 @@ import '../entites/user_entity.dart';
 
 abstract class AuthRepo
 {
-       Future<Either<Failure,List<UserEntity>>>postUserData();
-       Future<Either<Failure,List<UserEntity>>>loginWithGoogle();
+       Future<Either<Failure,UserEntity>>postUserData();
+       Future<Either<Failure,UserEntity>>loginWithGoogle();
 }
