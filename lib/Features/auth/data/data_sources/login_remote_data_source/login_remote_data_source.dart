cf12d4a47a6150cb0fa2d7b25a733e79edb6f699
hassan <hk5708102@gmail.com>
2024-03-12 18:47:34 +0200

    login_remote_data_source without impl

--- lib/Features/auth/data/data_sources/login_remote_data_source/login_remote_data_source.dart
+++ lib/Features/auth/data/data_sources/login_remote_data_source/login_remote_data_source.dart
@@ -1,33 +1,23 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
-//
-// import '../../../../../core/utils/app_router/app_router.dart';
-//
-// abstract class LoginRemoteDataSource
-// {
-//  Future signInWithGoogle(BuildContext context);
-//
-// }
-// class LoginRemoteDataImple extends LoginRemoteDataSource
-// {
-//   Future signInWithGoogle(BuildContext context) async {
-//     // Trigger the authentication flow
-//     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-//
-//     // Obtain the auth details from the request
-//     final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-//
-//     // Create a new credential
-//     final credential = GoogleAuthProvider.credential(
-//       accessToken: googleAuth?.accessToken,
-//       idToken: googleAuth?.idToken,
-//     );
-//
-//     // Once signed in, return the UserCredential
-//     await FirebaseAuth.instance.signInWithCredential(credential);
-//     Navigator.of(context).pushNamedAndRemoveUntil(Onboarding, (route) => false);
-//
-//     // addCateogry();
-//   }
-// }
+ import '../../../domain/entites/user_entity.dart';
+
+abstract class LoginRemoteDataSource
+ {
+
+    Future<UserEntity>postUserData();
+    Future<UserEntity>loginWithGoogle();
+ }
+ class LoginRemoteDataSourceImpl extends LoginRemoteDataSource
+ {
+  @override
+  Future<UserEntity> loginWithGoogle() {
+    // TODO: implement loginWithGoogle
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<UserEntity> postUserData() {
+    // TODO: implement postUserData
+    throw UnimplementedError();
+  }
+
+ }
